d7c880224e49b4175a69dd1fe052f770a48b9e7e
xmmmmmovo <13256172360@163.com>
2020-01-08 14:16:59 +0800

    2020.01.08 [修改部分界面]

--- app/src/utils/request.ts
+++ app/src/utils/request.ts
@@ -2,11 +2,13 @@ import axios from 'axios'
 import Qs from 'qs'
 import {Message} from "element-ui";
 
+// 初始化axios并将其注册为service
 const service = axios.create({
     baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
     timeout: 5000
 })
 
+// 跨域请求使用的请求拦截器 但是我在后端做了 就不用前端了
 // Request interceptors
 // service.interceptors.request.use(
 //   (config) => {
@@ -18,11 +20,12 @@ const service = axios.create({
 //   }
 // )
 
-// Response interceptors
+// 返回的拦截器 用于拦截返回码之类的 将纯数据返回
 service.interceptors.response.use(
     (response) => {
         const res = response.data
         if (res.code !== 200) {
+            // 如果状态码不是200 则说明有错误
             if (res.code === 500 || res.code === 400) {
                 Message({
                     message: res.message || 'Error',
@@ -32,11 +35,15 @@ service.interceptors.response.use(
                 })
                 location.reload() // To prevent bugs from vue-router
             }
+            // 拒绝本次请求
             return Promise.reject(new Error(res.message || 'Error'))
         } else {
+            // 如果没有拒绝(请求码是200
+            // 则将有用的data数据返回
             return response.data
         }
     },
+    // 同时 我们也需要判断是否超时 是否拒绝访问了
     (error) => {
         Message({
             message: error.message,
