2f22bdf386cb001e9eb8a992a445601be91b7b84
xmmmmmovo <13256172360@163.com>
2020-02-01 21:45:38 +0800

    mapper

--- api/src/main/java/org/nuc/course/db/dao/ArtMapper.java
+++ api/src/main/java/org/nuc/course/db/dao/ArtMapper.java
@@ -15,55 +15,12 @@ public interface ArtMapper extends Mapper<Art> {
 
     List<ArtDTO> findUserDTO(@Param("id") Long id);
 
-    @Select("select\n" +
-            "       art.id,\n" +
-            "       art_name as artName,\n" +
-            "       create_time as createTime,\n" +
-            "       modified_time as modifiedTime,\n" +
-            "       art_author as artAuthor,\n" +
-            "       user_name as userName,\n" +
-            "       art_size as artSize,\n" +
-            "       length,\n" +
-            "       height,\n" +
-            "       art_tag as artTag,\n" +
-            "       tag_name as tagName,\n" +
-            "       art_like_num as artLikeNum,\n" +
-            "       art_comment_num as artCommentNum,\n" +
-            "       art_content as artContent,\n" +
-            "       art_pic_url as artPicUrl\n" +
-            "from art\n" +
-            "    left join tag t on art.art_tag = t.id\n" +
-            "    left join size s on art.art_size = s.id\n" +
-            "    left join user u on art.art_author = u.id\n" +
-            "where art.id = ${id}")
+
     ArtDTO findIdDTO(@Param("id") Long id);
 
-    @Select("select\n" +
-            "       art.id,\n" +
-            "       art_name as artName,\n" +
-            "       create_time as createTime,\n" +
-            "       modified_time as modifiedTime,\n" +
-            "       art_author as artAuthor,\n" +
-            "       user_name as userName,\n" +
-            "       art_size as artSize,\n" +
-            "       length,\n" +
-            "       height,\n" +
-            "       art_tag as artTag,\n" +
-            "       tag_name as tagName,\n" +
-            "       art_like_num as artLikeNum,\n" +
-            "       art_comment_num as artCommentNum,\n" +
-            "       art_content as artContent,\n" +
-            "       art_pic_url as artPicUrl\n" +
-            "from art\n" +
-            "    left join tag t on art.art_tag = t.id\n" +
-            "    left join size s on art.art_size = s.id\n" +
-            "    left join user u on art.art_author = u.id\n" +
-            "where art.art_name like '%${name}%'")
     List<ArtDTO> findUserLikeDTO(@Param("name") String name);
 
-    @Update("update art set art_comment_num = art_comment_num + 1 where id = ${id}")
     void updateCommentCount(@Param("id") Long id);
 
-    @Update("update art set art_like_num = art_like_num + 1 where id = ${id}")
     void updateLikeCount(@Param("id") Long id);
 }
--- api/src/main/java/org/nuc/course/db/dao/ArticleMapper.java
+++ api/src/main/java/org/nuc/course/db/dao/ArticleMapper.java
@@ -10,38 +10,9 @@ import java.util.List;
 
 public interface ArticleMapper extends Mapper<Article> {
 
-    @Select("select article.id,\n" +
-            "       article_author as articleAuthor,\n" +
-            "       article_title as articleTitle,\n" +
-            "       article_content as articleContent,\n" +
-            "       article_front as articleFront,\n" +
-            "       article_author as articleAuthor,\n" +
-            "       create_time as createTime,\n" +
-            "       user_name as userName\n" +
-            "from article left join user u on article.article_author = u.id")
     List<ArticleDTO> findAllDTO();
 
-    @Select("select article.id,\n" +
-            "       article_author as articleAuthor,\n" +
-            "       article_title as articleTitle,\n" +
-            "       article_content as articleContent,\n" +
-            "       article_front as articleFront,\n" +
-            "       article_author as articleAuthor,\n" +
-            "       create_time as createTime,\n" +
-            "       user_name as userName\n" +
-            "from article left join user u on article.article_author = u.id\n" +
-            "where u.id = ${id}")
     List<ArticleDTO> findArticleDTO(@Param("id") Long id);
 
-    @Select("select article.id,\n" +
-            "       article_author as articleAuthor,\n" +
-            "       article_title as articleTitle,\n" +
-            "       article_content as articleContent,\n" +
-            "       article_front as articleFront,\n" +
-            "       article_author as articleAuthor,\n" +
-            "       create_time as createTime,\n" +
-            "       user_name as userName\n" +
-            "from article left join user u on article.article_author = u.id\n" +
-            "where article.id = ${id}")
     ArticleDTO findIdDTO(@Param("id") Long id);
 }
