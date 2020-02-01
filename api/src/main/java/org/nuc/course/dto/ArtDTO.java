8433597f0dad25b0430fa719974322fa05ffcba3
xmmmmmovo <13256172360@163.com>
2020-02-01 19:22:40 +0800

    mapper

--- api/src/main/java/org/nuc/course/dto/ArtDTO.java
+++ api/src/main/java/org/nuc/course/dto/ArtDTO.java
@@ -2,11 +2,14 @@ package org.nuc.course.dto;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
 public class ArtDTO {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String artName;
     private Long createTime;
